ec9237da8353a6150580721f31c0b7dc595f7311
yungacco <isaccofolegnani@gmail.com>
2024-02-27 20:19:26 +0100

    fix1

--- script.js
+++ script.js
@@ -6,13 +6,23 @@ for (let i = 0; i < 3; i++) {
 
   pedineContainer.appendChild(pedina);
 
+  let isMouseDown = false;
+
   pedina.addEventListener("mousedown", (e) => {
+    isMouseDown = true;
+
     const offsetX = e.clientX - pedina.offsetLeft;
     const offsetY = e.clientY - pedina.offsetTop;
 
     document.addEventListener("mousemove", (e) => {
-      pedina.style.left = `${e.clientX - offsetX}px`;
-      pedina.style.top = `${e.clientY - offsetY}px`;
+      if (isMouseDown) {
+        pedina.style.left = `${e.clientX - offsetX}px`;
+        pedina.style.top = `${e.clientY - offsetY}px`;
+      }
     });
+  });
+
+  pedina.addEventListener("mouseup", () => {
+    isMouseDown = false;
   });
 }
